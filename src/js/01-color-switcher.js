8a7c562e4dad93e33611f1ee5d85e32ee6863a3a
Michal901 <momot1122@wp.pl>
2024-02-06 11:47:21 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let swapperId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  swapperId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log(getRandomHexColor());
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(swapperId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
